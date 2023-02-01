7ee11618534ec012f95e2a4a10ea9f42bf05afca
hoangp2 <hoangp2@lasalle.edu>
2023-02-01 13:50:42 -0500

    WIP - Don't Merge

--- RestaurantWebsite/Program.cs
+++ RestaurantWebsite/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestaurantWebsite;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,15 @@ using System.Threading.Tasks;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+startup.Configure(app, builder.Environment);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
--- RestaurantWebsite/Startup.cs
+++ RestaurantWebsite/Startup.cs
@@ -16,16 +16,17 @@ namespace RestaurantWebsite
 {
     public class Startup
     {
+        public IConfiguration configRoot
+        {
+            get;
+        }
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            configRoot = configuration;
         }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages();
             services.AddControllersWithViews();
             services.AddRouting(options => {
                 options.LowercaseUrls = true;
@@ -33,35 +34,22 @@ namespace RestaurantWebsite
             });
             services.AddDbContext<FoodContext>(options =>
             options.UseSqlServer(
-                Configuration.GetConnectionString("FoodContext")));
+                configRoot.GetConnectionString("FoodContext")));
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(WebApplication app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (!app.Environment.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.MapRazorPages();
+            app.Run();
         }
     }
 }
