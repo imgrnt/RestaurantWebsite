c64ad481e6c0e864f9c8f62d03f6b813c4c35340
Ithan sandoval <ithansandoval@Ithans-MacBook-Pro.local>
2023-02-13 15:25:26 -0500

    Added the ability to make a reservation; which adds it into a database

---
+++ RestaurantWebsite/Models/Reservation.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantWebsite.Models
+{
+    public class Food
+    {
+        [Key]
+        public int MenuId { get; set; }
+
+        public string? Name { get; set; }
+
+        public double? Price { get; set; }
+
+
+    }
+}
