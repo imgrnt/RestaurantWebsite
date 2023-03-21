5f2d52a10e1867f0c932606f20dd8c7c79480f6c
ithansandoval <ithan02@gmail.com>
2023-03-21 10:40:51 -0400

    Login works now

---
+++ RestaurantWebsite/Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantWebsite.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Please enter a username.")]
+        [StringLength(255)]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password.")]
+        [StringLength(255)]
+        public string? Password { get; set; }
+
+
+    }
+}
+
